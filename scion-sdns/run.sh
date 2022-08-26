96965654d3bd2b84c4dcf6da1695cde192e0cf53
Thorben Krüger <thorben.krueger@ovgu.de>
2022-08-26 16:09:48 +0200

    better sdns timeout

--- scion-sdns/run.sh
+++ scion-sdns/run.sh
@@ -3,8 +3,8 @@ set -e
 # TODO: Fail if dispatcher or daemon crash
 /root/dispatcher --config $1 &
 /root/daemon --config $2 &
-echo "Waiting 30 seconds for dispatcher to come up before attempting to start SDNS"
-sleep 30;
+echo "Waiting 45 seconds for dispatcher to come up before attempting to start SDNS"
+sleep 45;
 echo "Starting SDNS"
 while sleep 1
 do /root/scion-sdns -config=$3
