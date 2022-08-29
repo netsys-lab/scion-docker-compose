483cc8651be4feb9a5f30c98b3fd4be0f7c84e16
Thorben Krüger <thorben.krueger@ovgu.de>
2022-08-29 17:41:01 +0200

    modify sdns and coredns runners

--- scion-coredns/run.sh
+++ scion-coredns/run.sh
@@ -2,10 +2,9 @@
 set -e
 # TODO: Fail if dispatcher or daemon crash
 /root/dispatcher --config $1 &
-/root/daemon --config $2 &
 echo "Waiting 30 seconds for dispatcher to come up before attempting to start coredns"
 sleep 30;
 echo "Starting Coredns"
 while sleep 1
-do /root/scion-coredns -conf $3
+do /root/scion-coredns -conf $2
 done
