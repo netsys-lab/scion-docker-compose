9e60fc91ae5945485864568005479561a9bda8fe
Thorben Krüger <thorben.krueger@ovgu.de>
2022-08-30 16:47:22 +0200

    marginal improvements

--- daemon/run.sh
+++ daemon/run.sh
@@ -1,5 +1,11 @@
 # !/bin/bash
 set -e
-# TODO: Fail if dispatcher crashes
-/root/dispatcher --config $1 & 
+/root/dispatcher --config $1 &
+pid1=$!
 /root/daemon --config $2 
+pid2=$!
+
+# terminate script if either command terminates
+wait -n $pid1 $pid2
+# send kill signal to both pids since we don't know which one terminated
+kill $pid1 $pid2
